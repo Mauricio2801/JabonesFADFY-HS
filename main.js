5ee7516e12e2682f737662fbfacb23387caab2c3
gabrielegr <00199518@uca.edu.sv>
2021-04-09 13:34:45 -0600

    Seccion Productos

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-    document.getElementById("about").onclick = function() {mostrar()};
+    document.getElementById("about").onclick = function() {mostrar_about()};
 
-    function mostrar() {
+    document.getElementById("products").onclick = function() {mostrar_productos()};
+
+    function mostrar_about() {
         document.getElementById("about-us").style.display="block";
+        document.getElementById("products-container").style.display="none";
     }
+    function mostrar_productos() {
+        document.getElementById("products-container").style.display="block";
+        document.getElementById("about-us").style.display="none";
+    }
+
+
+
+//Desaparecer imagen de producto cuando se pone mouse encima
+
+var product_image = document.getElementsByClassName("product-image");
+var fader = document.getElementsByClassName("fader");
+
+
+function toggle(){
+    for (var i = 0 ; i < fader.length; i++) {
+        fader[i].classList.toggle("fade"); 
+    }
+    
+}
+
+for (var i = 0 ; i < product_image.length; i++) {
+    product_image[i].addEventListener('mouseover' , toggle , false ) ; 
+    product_image[i].addEventListener('mouseout' , toggle , false ) ; 
+}
+
