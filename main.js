3065ea0386e1bf0cef95c2d7a57af92651936789
gabrielegr <00199518@uca.edu.sv>
2021-04-10 15:01:07 -0600

    test

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
-    document.getElementById("about").onclick = function() {mostrar_about()};
-    document.getElementById("close").onclick = function() {cerrar_about()};
-    document.getElementById("controler-info").onclick = function() {mostrar_info()};
-    document.getElementById("controler-info1").onclick = function() {mostrar_info1()};
-    document.getElementById("controler-info2").onclick = function() {mostrar_info2()};
-    document.getElementById("controler-info3").onclick = function() {mostrar_info3()};
+document.getElementById("about").onclick = function () { mostrar_about() };
+var closer = document.getElementsByClassName("close");
 
-    document.getElementById("products").onclick = function() {mostrar_productos()};
+for (var i = 0; i < closer.length; i++) {
+    closer[i].onclick = function () { cerrar() };
+}
 
-    function mostrar_about() {
-        document.getElementById("about-us").style.display="block";
-        document.getElementById("products-container").style.display="none";
-        document.getElementById("main-container").style.display="none";
-    }
-    function mostrar_info(){
-        document.getElementById("info").style.display="block";
-    }
-    function mostrar_info1(){
-        document.getElementById("info1").style.display="block";
-    }
-    function mostrar_info2(){
-        document.getElementById("info2").style.display="block";
-    }
-    function mostrar_info3(){
-        document.getElementById("info3").style.display="block";
-    }
 
-    function mostrar_productos() {
-        document.getElementById("products-container").style.display="block";
-        document.getElementById("about-us").style.display="none";
-        document.getElementById("main-container").style.display="none";
-    }
-    function cerrar_about(){
-        document.getElementById("about-us").style.display="none";
-        document.getElementById("products-container").style.display="none";
-        document.getElementById("main-container").style.display="block";
-    }
+
+document.getElementById("controler-info").onclick = function () { mostrar_info() };
+document.getElementById("controler-info1").onclick = function () { mostrar_info1() };
+document.getElementById("controler-info2").onclick = function () { mostrar_info2() };
+document.getElementById("controler-info3").onclick = function () { mostrar_info3() };
+
+document.getElementById("products").onclick = function () { mostrar_productos() };
+
+function mostrar_about() {
+    document.getElementById("about-us").style.display = "block";
+    document.getElementById("products-container").style.display = "none";
+    document.getElementById("main-container").style.display = "none";
+}
+function mostrar_info() {
+    document.getElementById("info").style.display = "block";
+}
+function mostrar_info1() {
+    document.getElementById("info1").style.display = "block";
+}
+function mostrar_info2() {
+    document.getElementById("info2").style.display = "block";
+}
+function mostrar_info3() {
+    document.getElementById("info3").style.display = "block";
+}
+
+function mostrar_productos() {
+    document.getElementById("products-container").style.display = "block";
+    document.getElementById("about-us").style.display = "none";
+    document.getElementById("main-container").style.display = "none";
+}
+function cerrar() {
+    document.getElementById("about-us").style.display = "none";
+    document.getElementById("products-container").style.display = "none";
+    document.getElementById("main-container").style.display = "block";
+}
+
 
 
 //Desaparecer imagen de producto cuando se pone mouse encima
@@ -43,15 +51,15 @@ var product_image = document.getElementsByClassName("product-image");
 var fader = document.getElementsByClassName("fader");
 
 
-function toggle(){
-    for (var i = 0 ; i < fader.length; i++) {
-        fader[i].classList.toggle("fade"); 
+function toggle() {
+    for (var i = 0; i < fader.length; i++) {
+        fader[i].classList.toggle("fade");
     }
-    
+
 }
 
-for (var i = 0 ; i < product_image.length; i++) {
-    product_image[i].addEventListener('mouseover' , toggle , false ) ; 
-    product_image[i].addEventListener('mouseout' , toggle , false ) ; 
+for (var i = 0; i < product_image.length; i++) {
+    product_image[i].addEventListener('mouseover', toggle, false);
+    product_image[i].addEventListener('mouseout', toggle, false);
 }
 
